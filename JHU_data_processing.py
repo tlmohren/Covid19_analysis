589e889aa80f6e5318db2f24446d694610b43ae6
tlmohren <tlmohren@uw.edu>
2020-04-13 09:38:49 -0700

    moved data processing functions to new file

---
+++ JHU_data_processing.py
@@ -0,0 +1,295 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates  
+import numpy as np
+from datetime import timedelta
+from matplotlib.dates import date2num       #-->Update 
+from matplotlib.colors import ListedColormap
+import json
+import matplotlib.dates as dt    
+import geopandas as gpd
+import datetime
+# import geopandas as gpd
+import shapely.affinity as shp
+import glob
+
+
+state_dict = {
+        'AK': 'Alaska',
+        'AL': 'Alabama',
+        'AR': 'Arkansas',
+        'AS': 'American Samoa',
+        'AZ': 'Arizona',
+        'CA': 'California',
+        'CO': 'Colorado',
+        'CT': 'Connecticut',
+        'DC': 'District of Columbia',
+        'DE': 'Delaware',
+        'FL': 'Florida',
+        'GA': 'Georgia',
+        'GU': 'Guam',
+        'HI': 'Hawaii',
+        'IA': 'Iowa',
+        'ID': 'Idaho',
+        'IL': 'Illinois',
+        'IN': 'Indiana',
+        'KS': 'Kansas',
+        'KY': 'Kentucky',
+        'LA': 'Louisiana',
+        'MA': 'Massachusetts',
+        'MD': 'Maryland',
+        'ME': 'Maine',
+        'MI': 'Michigan',
+        'MN': 'Minnesota',
+        'MO': 'Missouri',
+        'MP': 'Northern Mariana Islands',
+        'MS': 'Mississippi',
+        'MT': 'Montana',
+        'NA': 'National',
+        'NC': 'North Carolina',
+        'ND': 'North Dakota',
+        'NE': 'Nebraska',
+        'NH': 'New Hampshire',
+        'NJ': 'New Jersey',
+        'NM': 'New Mexico',
+        'NV': 'Nevada',
+        'NY': 'New York',
+        'OH': 'Ohio',
+        'OK': 'Oklahoma',
+        'OR': 'Oregon',
+        'PA': 'Pennsylvania',
+        'PR': 'Puerto Rico',
+        'RI': 'Rhode Island',
+        'SC': 'South Carolina',
+        'SD': 'South Dakota',
+        'TN': 'Tennessee',
+        'TX': 'Texas',
+        'UT': 'Utah',
+        'VA': 'Virginia',
+        'VI': 'Virgin Islands',
+        'VT': 'Vermont',
+        'WA': 'Washington',
+        'WI': 'Wisconsin',
+        'WV': 'West Virginia',
+        'WY': 'Wyoming'
+}
+
+
+
+def replace_state( x):
+    
+    error_dict = {'Chicago':'Illinois',
+            'NE (From Diamond Princess)':'Nebraska',
+            'CA (From Diamond Princess)':'California',
+            'TX (From Diamond Princess)':'Texas',
+            'Unassigned Location (From Diamond Princess)':'Other',
+            'D.C.':'District of Columbia',
+            'Puerto Rico':'Other',
+            'Guam':'Other',
+            'U.S.':'Other',
+            'US':'Other',
+            'Virgin Islands':'Other',
+            'United States Virgin Islands':'Other',
+            'Wuhan Evacuee':'Other',
+            'American Samoa':'Other',
+            'Northern Mariana Islands':'Other', 
+            'OR ':'Oregon'} 
+    
+    state_entry = x['Province/State'].split(", ")[-1] 
+    if state_entry in  state_dict.keys(): 
+        new =  state_dict[ state_entry]  
+    elif state_entry in error_dict.keys():
+        new = error_dict[state_entry]  
+    else:
+        new = state_entry   
+    return new 
+
+
+def load_daily_reports( file_path):
+    
+    daily_reports = glob.glob(file_path + '\*.csv') 
+    df = pd.DataFrame()
+    for file in daily_reports :
+        df_temp = pd.read_csv( file, index_col=None, header=0)  
+
+        if  '03-13-2020' in file :
+            # correct for mistake in JHU data set
+            df_temp['Last Update'] = pd.to_datetime(df_temp['Last Update']   )
+            df_temp['Last Update'] = df_temp['Last Update'] + timedelta(days=2)  
+
+        if  '03-09-2020' in file : 
+            # correct for mistake in JHU data set
+            df_temp['Last Update'] = pd.to_datetime(df_temp['Last Update']   )
+            df_temp['Last Update'] = df_temp['Last Update'] + timedelta(days=1) 
+
+        df = df.append(df_temp, sort=True)  
+    return df
+
+
+def process_daily_data( df_daily):
+    # convert both separately 
+    df_daily['Last Update'] = pd.to_datetime(df_daily['Last Update']   )
+    df_daily['Last_Update'] = pd.to_datetime(df_daily['Last_Update']   )   # 
+    cond = df_daily['Last_Update'].isnull()
+    df_daily['Last Update'] = df_daily['Last Update'].where(cond, df_daily['Last_Update'] ) 
+    df_daily['Datetime'] = pd.to_datetime(df_daily['Last Update'] , unit='D'   ) 
+
+    ### optional: convert times to pacific time 
+    df_daily['Date'] = df_daily['Datetime'].dt.date 
+
+    # compensate country/region
+    df_daily['Country/Region'].isnull().sum() 
+    cond = df_daily['Country_Region'].isnull()
+    df_daily['Country/Region'] = df_daily['Country/Region'].where(cond, df_daily['Country_Region'] ) 
+
+    # province state
+    cond = df_daily['Province_State'].isnull()
+    df_daily['Province/State'] = df_daily['Province/State'].where(cond, df_daily['Province_State'] ) 
+
+
+    col_order = ['Date','Country/Region','Province/State','Active','Confirmed','Deaths','Recovered' ]
+    df_daily = df_daily[col_order]
+    
+    bool_US = df_daily['Country/Region'] =='US'
+    df_US = df_daily[bool_US ]
+
+    df_US.loc[:,'State'] = df_US.copy().apply( replace_state, axis=1)
+    
+    df_state =  df_US.groupby(['State','Date']).sum().reset_index()
+  
+    df_state = df_state.sort_values(by=['State','Date'], ascending=True )  
+    df_state.head() 
+ 
+    # drop some states
+    bool_other = df_state['State'].str.contains( 'Other' , regex=False) 
+    df_state.drop( df_state[bool_other].index, inplace=True)  
+  
+    bool_other = df_state['State'].str.contains( 'Princess' , regex=False) 
+    df_state.drop( df_state[bool_other].index, inplace=True)  
+
+    return df_state 
+
+
+def merge_countrydf(path, file_list):
+    
+    df_list = []
+    for file in file_list: 
+        df_list.append( pd.read_csv( path + file , index_col=None, header=0) )
+        
+    df = pd.DataFrame()
+    date_cols = df_list[0].columns[  4:  ]
+
+    # for date_col in date_cols[:]: 
+    for i in range(len(date_cols)) : 
+        col_index = df_list[0].columns[ [1 ] ].append( date_cols[[i]] ) 
+        df_temp = df_list[0][col_index].copy() 
+        df_temp[ 'Death'] = df_list[1][date_cols[[i]] ]  
+        df_temp[ 'Recovered'] = df_list[2][date_cols[[i]] ]  # works but gives warning  
+        df_temp['Date'] = date_cols[i]
+        df_temp = df_temp.rename( columns={date_cols[i]:'Confirmed'})    
+        df = df.append(df_temp)     
+        
+    df = df.groupby(['Country/Region','Date']).sum().reset_index()
+    
+    df['Date'] = pd.to_datetime( df['Date'] ).dt.date 
+        
+    df = df.sort_values( by=['Country/Region','Date']).reset_index(drop=True) 
+
+    return df
+ 
+
+def days_since_threshold( date_series, value_series, region_series, threshold_val ) : 
+    
+    # find all that belong to last day
+    last_day = date_series.max() 
+     
+    # find unique entries: 
+    countries = region_series.unique() 
+        
+    # initialize new column
+    newday_col = pd.Series(np.nan, index= region_series.index ) 
+    
+    for country in countries : 
+         
+        bool_country = (region_series == country).values
+        
+        x = date_series[bool_country] 
+        y = value_series[bool_country]  
+        
+        bool_threshold_cases = (y >= threshold_val)
+                                           
+        # if any time the cases reach above threshold
+        if bool_threshold_cases.sum() > 0: 
+            day0 = x[bool_threshold_cases].iloc[0] 
+        else: 
+            day0 = last_day + timedelta(days=1) 
+
+        newdays = ( x - day0).dt.days   
+        newday_col[bool_country] =  newdays.values
+          
+    return newday_col  
+
+
+def get_exponential_ratio( date_series, value_series, region_series, averaging_period ): 
+ 
+    last_day = date_series.max() 
+    prior_day = date_series.max() - timedelta(days=averaging_period)
+    bool_last = date_series == last_day
+    bool_prior = date_series == prior_day
+
+    new_ratio_col = pd.Series(np.nan, index= region_series.index  ) 
+
+    countries =  region_series.unique()
+    for country in countries: 
+        bool_country =  region_series == country   
+        
+        if value_series.loc[bool_last & bool_country ].values[0] == 0:
+            ratio = 0
+        elif value_series.loc[bool_prior & bool_country ].values[0] == 0:
+            ratio = np.inf
+        else:
+            ratio = value_series.loc[bool_last & bool_country ].values[0]  / \
+                    value_series.loc[bool_prior & bool_country ].values[0]  
+        daily_ratio = ratio**(1/averaging_period)   
+        new_ratio_col[bool_country] =  daily_ratio
+        
+    return new_ratio_col
+
+
+def add_measures_column( fileName, date_series, region_series ):
+
+	measures_col = pd.Series(np.nan, index= region_series.index  ) 
+
+	df_m = pd.read_csv( fileName, index_col=None ) 
+	df_m['Date'] = pd.to_datetime( df_m['Date'] ).dt.date 
+
+	# add measures to additional column
+	for i,row in df_m.iterrows(): 
+		if  'Country/Region' in  df_m.columns.tolist():
+			country = row['Country/Region']
+		elif 'State' in  df_m.columns.tolist():
+			country = row['State']
+		date = row['Date']
+		bool_loc = ( region_series == country) & ( date_series == date) 
+		measures_col.loc[bool_loc] = row['Measure']
+	return measures_col
+
+
+
+def find_daily_cases( date_series, value_series):
+    daily_col = pd.Series(np.nan, index= value_series.index  ) 
+    days = date_series.unique()   
+    
+    bool_day0 = date_series == days[0]
+    daily_col.loc[bool_day0 ] = 0 
+
+    for day in days[1:]:      
+
+        bool_day = date_series == day 
+        bool_prior = date_series == ( day- timedelta(days=1))  
+
+        delta = value_series[bool_day].values - value_series[bool_prior].values
+        
+        daily_col[bool_day] = delta 
+    return daily_col
+
--- JHUprocessing.py
+++ JHUprocessing.py
@@ -12,73 +12,14 @@ import datetime
 # import geopandas as gpd
 import shapely.affinity as shp
 import glob
+import JHU_data_processing as dp
 
 ####--------------------parameters --------------------------------------
 plot_country = True
 plot_states = True
 save_fig = True
-show_plot = False
-
-state_dict = {
-        'AK': 'Alaska',
-        'AL': 'Alabama',
-        'AR': 'Arkansas',
-        'AS': 'American Samoa',
-        'AZ': 'Arizona',
-        'CA': 'California',
-        'CO': 'Colorado',
-        'CT': 'Connecticut',
-        'DC': 'District of Columbia',
-        'DE': 'Delaware',
-        'FL': 'Florida',
-        'GA': 'Georgia',
-        'GU': 'Guam',
-        'HI': 'Hawaii',
-        'IA': 'Iowa',
-        'ID': 'Idaho',
-        'IL': 'Illinois',
-        'IN': 'Indiana',
-        'KS': 'Kansas',
-        'KY': 'Kentucky',
-        'LA': 'Louisiana',
-        'MA': 'Massachusetts',
-        'MD': 'Maryland',
-        'ME': 'Maine',
-        'MI': 'Michigan',
-        'MN': 'Minnesota',
-        'MO': 'Missouri',
-        'MP': 'Northern Mariana Islands',
-        'MS': 'Mississippi',
-        'MT': 'Montana',
-        'NA': 'National',
-        'NC': 'North Carolina',
-        'ND': 'North Dakota',
-        'NE': 'Nebraska',
-        'NH': 'New Hampshire',
-        'NJ': 'New Jersey',
-        'NM': 'New Mexico',
-        'NV': 'Nevada',
-        'NY': 'New York',
-        'OH': 'Ohio',
-        'OK': 'Oklahoma',
-        'OR': 'Oregon',
-        'PA': 'Pennsylvania',
-        'PR': 'Puerto Rico',
-        'RI': 'Rhode Island',
-        'SC': 'South Carolina',
-        'SD': 'South Dakota',
-        'TN': 'Tennessee',
-        'TX': 'Texas',
-        'UT': 'Utah',
-        'VA': 'Virginia',
-        'VI': 'Virgin Islands',
-        'VT': 'Vermont',
-        'WA': 'Washington',
-        'WI': 'Wisconsin',
-        'WV': 'West Virginia',
-        'WY': 'Wyoming'
-}
-
+show_plot = True
+ 
 # figure size
 full_w = (12,7)
 half_w = (6,4) 
@@ -126,131 +67,6 @@ dot_alpha = 1
 
 
 ####------------------functions-- --------------------------------------
-def merge_countrydf(path, file_list):
-    
-    df_list = []
-    for file in file_list: 
-        df_list.append( pd.read_csv( path + file , index_col=None, header=0) )
-        
-    df = pd.DataFrame()
-    date_cols = df_list[0].columns[  4:  ]
-
-    # for date_col in date_cols[:]: 
-    for i in range(len(date_cols)) : 
-        col_index = df_list[0].columns[ [1 ] ].append( date_cols[[i]] ) 
-        df_temp = df_list[0][col_index].copy() 
-        df_temp[ 'Death'] = df_list[1][date_cols[[i]] ]  
-        df_temp[ 'Recovered'] = df_list[2][date_cols[[i]] ]  # works but gives warning  
-        df_temp['Date'] = date_cols[i]
-        df_temp = df_temp.rename( columns={date_cols[i]:'Confirmed'})    
-        df = df.append(df_temp)     
-        
-    df = df.groupby(['Country/Region','Date']).sum().reset_index()
-    
-    df['Date'] = pd.to_datetime( df['Date'] ).dt.date 
-        
-    df = df.sort_values( by=['Country/Region','Date']).reset_index(drop=True) 
-
-    return df
-
-
-####------------------compute days since threshold-------------------------------------
-def days_since_threshold( date_series, value_series, region_series, threshold_val ) : 
-    
-    # find all that belong to last day
-    last_day = date_series.max() 
-     
-    # find unique entries: 
-    countries = region_series.unique() 
-        
-    # initialize new column
-    newday_col = pd.Series(np.nan, index= region_series.index ) 
-    
-    for country in countries : 
-         
-        bool_country = (region_series == country).values
-        
-        x = date_series[bool_country] 
-        y = value_series[bool_country]  
-        
-        bool_threshold_cases = (y >= threshold_val)
-                                           
-        # if any time the cases reach above threshold
-        if bool_threshold_cases.sum() > 0: 
-            day0 = x[bool_threshold_cases].iloc[0] 
-        else: 
-            day0 = last_day + timedelta(days=1) 
-
-        newdays = ( x - day0).dt.days   
-        newday_col[bool_country] =  newdays.values
-          
-    return newday_col  
-
-
-def get_exponential_ratio( date_series, value_series, region_series, averaging_period ): 
- 
-    last_day = date_series.max() 
-    prior_day = date_series.max() - timedelta(days=averaging_period)
-    bool_last = date_series == last_day
-    bool_prior = date_series == prior_day
-
-    new_ratio_col = pd.Series(np.nan, index= region_series.index  ) 
-
-    countries =  region_series.unique()
-    for country in countries: 
-        bool_country =  region_series == country   
-        
-        if value_series.loc[bool_last & bool_country ].values[0] == 0:
-            ratio = 0
-        elif value_series.loc[bool_prior & bool_country ].values[0] == 0:
-            ratio = np.inf
-        else:
-            ratio = value_series.loc[bool_last & bool_country ].values[0]  / \
-                    value_series.loc[bool_prior & bool_country ].values[0]  
-        daily_ratio = ratio**(1/averaging_period)   
-        new_ratio_col[bool_country] =  daily_ratio
-        
-    return new_ratio_col
-
-
-def add_measures_column( fileName, date_series, region_series ):
-
-	measures_col = pd.Series(np.nan, index= region_series.index  ) 
-
-	df_m = pd.read_csv( fileName, index_col=None ) 
-	df_m['Date'] = pd.to_datetime( df_m['Date'] ).dt.date 
-
-	# add measures to additional column
-	for i,row in df_m.iterrows(): 
-		if  'Country/Region' in  df_m.columns.tolist():
-			country = row['Country/Region']
-		elif 'State' in  df_m.columns.tolist():
-			country = row['State']
-		date = row['Date']
-		bool_loc = ( region_series == country) & ( date_series == date) 
-		measures_col.loc[bool_loc] = row['Measure']
-	return measures_col
-
-
-
-def find_daily_cases( date_series, value_series):
-    daily_col = pd.Series(np.nan, index= value_series.index  ) 
-    days = date_series.unique()   
-    
-    bool_day0 = date_series == days[0]
-    daily_col.loc[bool_day0 ] = 0 
-
-    for day in days[1:]:     
-        # print(days[0] , day- timedelta(days=1))
-
-        bool_day = date_series == day 
-        bool_prior = date_series == ( day- timedelta(days=1))  
-
-        delta = value_series[bool_day].values - value_series[bool_prior].values
-        
-        daily_col[bool_day] = delta 
-    return daily_col
-
 
 
 
@@ -390,107 +206,8 @@ def plot_daily( ax_p, date_col, data_series, measures = pd.DataFrame() ):
         counter = counter+1 
     return ax_p 
 
-	    
-def load_daily_reports( file_path):
-    
-    daily_reports = glob.glob(file_path + '\*.csv') 
-    df = pd.DataFrame()
-    for file in daily_reports :
-        df_temp = pd.read_csv( file, index_col=None, header=0)  
-
-        if  '03-13-2020' in file :
-            # correct for mistake in JHU data set
-            df_temp['Last Update'] = pd.to_datetime(df_temp['Last Update']   )
-            df_temp['Last Update'] = df_temp['Last Update'] + timedelta(days=2)  
-
-        if  '03-09-2020' in file : 
-            # correct for mistake in JHU data set
-            df_temp['Last Update'] = pd.to_datetime(df_temp['Last Update']   )
-            df_temp['Last Update'] = df_temp['Last Update'] + timedelta(days=1) 
-
-        df = df.append(df_temp, sort=True)  
-    return df
 
 
-
-
-
-
-def replace_state( x):
-    
-    error_dict = {'Chicago':'Illinois',
-            'NE (From Diamond Princess)':'Nebraska',
-            'CA (From Diamond Princess)':'California',
-            'TX (From Diamond Princess)':'Texas',
-            'Unassigned Location (From Diamond Princess)':'Other',
-            'D.C.':'District of Columbia',
-            'Puerto Rico':'Other',
-            'Guam':'Other',
-            'U.S.':'Other',
-            'US':'Other',
-            'Virgin Islands':'Other',
-            'United States Virgin Islands':'Other',
-            'Wuhan Evacuee':'Other',
-            'American Samoa':'Other',
-            'Northern Mariana Islands':'Other', 
-            'OR ':'Oregon'} 
-    
-    state_entry = x['Province/State'].split(", ")[-1] 
-    if state_entry in  state_dict.keys(): 
-        new =  state_dict[ state_entry]  
-    elif state_entry in error_dict.keys():
-        new = error_dict[state_entry]  
-    else:
-        new = state_entry   
-    return new 
-
-def process_daily_data( df_daily):
-    # convert both separately 
-    df_daily['Last Update'] = pd.to_datetime(df_daily['Last Update']   )
-    df_daily['Last_Update'] = pd.to_datetime(df_daily['Last_Update']   )   # 
-    cond = df_daily['Last_Update'].isnull()
-    df_daily['Last Update'] = df_daily['Last Update'].where(cond, df_daily['Last_Update'] ) 
-    df_daily['Datetime'] = pd.to_datetime(df_daily['Last Update'] , unit='D'   ) 
-
-    ### optional: convert times to pacific time 
-    df_daily['Date'] = df_daily['Datetime'].dt.date 
-
-
-    # compensate country/region
-    df_daily['Country/Region'].isnull().sum() 
-    cond = df_daily['Country_Region'].isnull()
-    df_daily['Country/Region'] = df_daily['Country/Region'].where(cond, df_daily['Country_Region'] ) 
-
-    # province state
-    cond = df_daily['Province_State'].isnull()
-    df_daily['Province/State'] = df_daily['Province/State'].where(cond, df_daily['Province_State'] ) 
-
-
-    col_order = ['Date','Country/Region','Province/State','Active','Confirmed','Deaths','Recovered' ]
-    df_daily = df_daily[col_order]
-    
-    bool_US = df_daily['Country/Region'] =='US'
-    df_US = df_daily[bool_US ]
-
-
-
-    df_US.loc[:,'State'] = df_US.copy().apply(replace_state, axis=1)
-    
-    
-    df_state =  df_US.groupby(['State','Date']).sum().reset_index()
-  
-    df_state = df_state.sort_values(by=['State','Date'], ascending=True )  
-    df_state.head() 
- 
-    # drop some states
-    bool_other = df_state['State'].str.contains( 'Other' , regex=False) 
-    df_state.drop( df_state[bool_other].index, inplace=True)  
-  
-    bool_other = df_state['State'].str.contains( 'Princess' , regex=False) 
-    df_state.drop( df_state[bool_other].index, inplace=True)  
-
-
-    return df_state 
 
 
 
@@ -511,19 +228,19 @@ if __name__ == '__main__':
 	         '\\time_series_covid19_recovered_global.csv'] 
 
 	# merge data 
-	df_country = merge_countrydf(path, file_list) 
+	df_country = dp.merge_countrydf(path, file_list) 
 
 	# remove diamond princess 
 	bool_other = df_country['Country/Region'].str.contains( 'Princess' , regex=False)
 	df_country.drop( df_country[bool_other].index, inplace=True) 
 
  	# compute delta days
-	df_country['Delta C'] =  days_since_threshold( df_country['Date'],
+	df_country['Delta C'] =  dp.days_since_threshold( df_country['Date'],
 	                                df_country['Confirmed'],
 	                                df_country['Country/Region'] , 
 	                                threshold_cases) 
 
-	df_country['Delta D'] = days_since_threshold( df_country['Date'],
+	df_country['Delta D'] = dp.days_since_threshold( df_country['Date'],
 	                                df_country['Death'],
 	                                df_country['Country/Region'] , 
 	                                threshold_deaths)
@@ -544,13 +261,13 @@ if __name__ == '__main__':
 	df_country.loc[bool_deaths & bool_china,'Delta D'] = xDmax+ china_add
 
 	# find ratio and doubling # days
-	df_country['ratio'] = get_exponential_ratio( df_country['Date'], 
+	df_country['ratio'] = dp.get_exponential_ratio( df_country['Date'], 
 	                                            df_country['Confirmed'],
 	                                            df_country['Country/Region'], 
 	                                            averaging_period )   
 	df_country['doubling'] = pd.cut( df_country['ratio'],   bin_array ,labels=range(len(bin_array)-1) , include_lowest=True )
 	 
-	df_country['ratioD'] = get_exponential_ratio( df_country['Date'], 
+	df_country['ratioD'] = dp.get_exponential_ratio( df_country['Date'], 
 	                                            df_country['Death'],
 	                                            df_country['Country/Region'], 
 	                                            averaging_period )  
@@ -558,11 +275,11 @@ if __name__ == '__main__':
 
  
  
-	df_country['Daily Confirmed'] = find_daily_cases( df_country['Date'] , df_country['Confirmed'] )
-	df_country['Daily Death'] = find_daily_cases( df_country['Date'] , df_country['Death'] )
+	df_country['Daily Confirmed'] = dp.find_daily_cases( df_country['Date'] , df_country['Confirmed'] )
+	df_country['Daily Death'] = dp.find_daily_cases( df_country['Date'] , df_country['Death'] )
  
 
-	df_country['Measure'] = add_measures_column( 'measures_per_country.csv', 
+	df_country['Measure'] = dp.add_measures_column( 'measures_per_country.csv', 
 	                                            df_country['Date'] ,
 	                                            df_country['Country/Region'])
 
@@ -611,9 +328,9 @@ if __name__ == '__main__':
 	# state data import and process  
 	daily_path = r'D:\Code_projects\Covid19_analysis\COVID-19\csse_covid_19_data\csse_covid_19_daily_reports'
 
-	df_daily = load_daily_reports( daily_path )
+	df_daily = dp.load_daily_reports( daily_path )
  
-	df_state = process_daily_data( df_daily ) 
+	df_state = dp.process_daily_data( df_daily ) 
 
 	# adjust california funk 
 	bool_cal = df_state['State'] == 'California'
@@ -648,12 +365,12 @@ if __name__ == '__main__':
 	threshold_cases = 100
 	threshold_deaths = 10
 
-	df_state['Delta C'] =   days_since_threshold( df_state['Date'],
+	df_state['Delta C'] =   dp.days_since_threshold( df_state['Date'],
 	                                df_state['Confirmed'],
 	                                df_state['State'] , 
 	                                threshold_cases) 
 
-	df_state['Delta D'] =  days_since_threshold( df_state['Date'],
+	df_state['Delta D'] =  dp.days_since_threshold( df_state['Date'],
 	                                df_state['Deaths'],
 	                                df_state['State'] , 
 	                                threshold_deaths)
@@ -664,7 +381,7 @@ if __name__ == '__main__':
 	# bin_array = np.array( [-1, 2.**(1./8), 2.**(1./7), 2.**(1./6), 2.**(1./5), 2.**(1./4) ,np.inf ]) 
 	# bin_tick_labels = ['8','7','6','5','4']
 
-	df_state['ratio'] =  get_exponential_ratio( df_state['Date'], 
+	df_state['ratio'] =  dp.get_exponential_ratio( df_state['Date'], 
 	                                            df_state['Confirmed'],
 	                                            df_state['State'], 
 	                                            averaging_period )   
@@ -672,7 +389,7 @@ if __name__ == '__main__':
 
 
 
-	df_state['ratioD'] =  get_exponential_ratio( df_state['Date'], 
+	df_state['ratioD'] =  dp.get_exponential_ratio( df_state['Date'], 
 	                                            df_state['Deaths'],
 	                                            df_state['State'], 
 	                                            averaging_period )  
@@ -691,12 +408,12 @@ if __name__ == '__main__':
 	df_state = df_state.sort_values( by=['State','Date']).reset_index(drop=True) 
 
 
-	df_state['Daily Confirmed'] = find_daily_cases( df_state['Date'] , df_state['Confirmed'] )
-	df_state['Daily Deaths'] = find_daily_cases( df_state['Date'] , df_state['Deaths'] )
+	df_state['Daily Confirmed'] = dp.find_daily_cases( df_state['Date'] , df_state['Confirmed'] )
+	df_state['Daily Deaths'] = dp.find_daily_cases( df_state['Date'] , df_state['Deaths'] )
  
 	# df_state['Date']  = df_state['Date'].apply(date2num)      #-->Update
 
-	df_state['Measure'] = add_measures_column( 'measures_per_state.csv', 
+	df_state['Measure'] = dp.add_measures_column( 'measures_per_state.csv', 
 	                                            df_state['Date'] ,
 	                                            df_state['State'])
 
